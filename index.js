d93873defd8c2d4e322fc9adbf9020e74287bf12
Lauren <lauren.m.lavelle@wmich.edu>
2023-08-16 20:07:14 -0400

    added schema for tables

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+// Connect to database
+// const db = mysql.createConnection(
+//     {
+//       host: 'localhost',
+//       // MySQL username,
+//       user: 'root',
+//       // MySQL password
+//       password: 'MSU#code1',
+//       database: ''
+//     },
+//     console.log(`Connected to the classlist_db database.`)
+//   );
+
+const startmenu =
+[
+    {type: "list",
+    name: "startMenu",
+    message: "Please choose an action to perform from the menu",
+    choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+    }
+]
 
 function init() {
-    inquirer.prompt()
-    // .then(response)=>       
+    inquirer.prompt(startmenu)
+    .then((response)=>{
+        // console.log(response.startMenu)
+        if (response.startMenu==="View all departments"){
+        console.log('response.startmenu')}
+        else{
+            console.log('hi')
+        }
+    })       
 }
 
-init()
+// init()
+
+// https://www.npmjs.com/package/mysql2
+// comments
+// README
